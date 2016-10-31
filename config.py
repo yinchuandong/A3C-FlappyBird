1e92f180548491b85e7cc19d94aa73f682d7ff6c
yinchuandong <johncdyin@gmail.com>
2016-10-31 15:23:51 +1100

    update: config

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ LOCAL_T_MAX = 5  # repeat step size
 RMSP_ALPHA = 0.99  # decay parameter for RMSProp
 RMSP_EPSILON = 0.1  # epsilon parameter for RMSProp
 GAMMA = 0.99
-LOCAL_T_MAX = 5
 ENTROPY_BETA = 0.01
 MAX_TIME_STEP = 10 * 10**7
 
@@ -16,7 +15,7 @@ INITIAL_ALPHA_HIGH = 1e-2   # log_uniform high limit for learning rate
 INITIAL_ALPHA_LOG_RATE = 0.4226  # log_uniform interpolate rate for learning rate (around 7 * 10^-4)
 
 PARALLEL_SIZE = 1  # parallel thread size
-USE_GPU = False
+USE_GPU = True
 USE_LSTM = True
 CHECKPOINT_DIR = 'checkpoints'
 
