0bc1c7cff7dc4d09ae7cb0fe52b77eef86f9e0ba
yinchuandong <johncdyin@gmail.com>
2016-11-12 10:57:20 +1100

    new: add udp socket

--- test_client.py
+++ test_client.py
@@ -2,21 +2,38 @@ import socket
 import sys
 import numpy as np
 import cPickle
+import time
 
 
 HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
-
-# SOCK_DGRAM is the socket type to use for UDP sockets
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# As you can see, there is no connect() call; UDP has no connections.
-# Instead, data is directly sent to the recipient via sendto().
-sock.sendto(data + "\n", (HOST, PORT))
-received = sock.recv(65535)
-data = cPickle.loads(received)
+
+def main():
+    data = " ".join(sys.argv[1:])
+
+    sock.sendto(data + "\n", (HOST, PORT))
+    header = sock.recv(1000)
+    header = cPickle.loads(header)
+    print header
+
+    data = str()
+    buffer_size = header["buffer_size"]
+    total_size = header["total_size"]
+    block_num = header["block_num"]
+    for i in range(block_num):
+        receive_size = total_size - len(data)
+        receive_size = receive_size if receive_size < buffer_size else buffer_size
+        data += sock.recv(receive_size)
+    data = cPickle.loads(data)
+
+    # print "Sent:     {}".format(data)
+    print "Received: {}".format(np.shape(data))
+
+    return
 
 
-print "Sent:     {}".format(data)
-print "Received: {}".format(np.shape(received))
-# print received
+if __name__ == '__main__':
+    for i in range(100):
+        main()
+        # time.sleep(1 / 30.0)
--- test_server.py
+++ test_server.py
@@ -1,32 +1,73 @@
-import SocketServer
+from SocketServer import BaseRequestHandler, UDPServer
 import numpy as np
 from PIL import Image
 import cPickle
+import math
+from game.game_state import GameState
 
 
-class MyUDPHandler(SocketServer.BaseRequestHandler):
-    """
-    This class works similar to the TCP handler class, except that
-    self.request consists of a pair of data and client socket, and since
-    there is no connection the client address must be given explicitly
-    when sending data back via sendto().
-    """
+gamestate = GameState()
+
+
+class MyUDPHandler(BaseRequestHandler):
+
+    def setup(self):
+        # print '---------setup'
+        # self.count = 1
+        return
 
     def handle(self):
-        # data = self.request[0].strip()
         socket = self.request[1]
-        # print "{} wrote:".format(self.client_address[0])
-        # print data
-        # socket.sendto(data.upper(), self.client_address)
-        image = Image.open('images/preprocess.png')
-        image = image.resize((120, 120), Image.ANTIALIAS)
-        image.save('images/test.png')
-        data = np.array(image)
-        bin_data = cPickle.dumps(data)
-        socket.sendto(bin_data, self.client_address)
+        # image = Image.open('images/preprocess.png')
+        # image = image.resize((120, 120), Image.ANTIALIAS)
+        # image.save('images/test.png')
+        # data = np.array(image)
+        # data = cPickle.dumps(data)
+        global gamestate
+        gamestate.process(1)
+        data = gamestate.s_t
+        data = cPickle.dumps(data)
+
+        # not larger than 8192 due to the limitation of MXU of udp
+        buffer_size = 8192
+        total_size = len(data)
+        block_num = int(math.ceil(total_size / float(buffer_size)))
+
+        # send the length
+        offset = 0
+        header = {
+            "buffer_size": buffer_size,
+            "total_size": total_size,
+            "block_num": block_num
+        }
+        header = cPickle.dumps(header)
+        socket.sendto(header, self.client_address)
+        while offset < total_size:
+            end = offset + buffer_size
+            end = end if end < total_size else total_size
+            socket.sendto(data[offset: end], self.client_address)
+            offset = end
+
+        # self.count += 1
+        # print self.count
+        return
+
+    def finish(self):
+        # print '---------finish'
+        # self.count = 0
+        # print self.count
+        return
+
+
+class MyUDPServer(UDPServer):
+    def __init__(self, server_address, handler_class=MyUDPHandler):
+        UDPServer.__init__(self, server_address, handler_class)
+        return
 
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-    server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
+    HOST, PORT = "localhost", 9998
+    server = MyUDPServer((HOST, PORT), MyUDPHandler)
     server.serve_forever()
+
+    # test()
