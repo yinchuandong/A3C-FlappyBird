c42bd43e943e189fbefc97f1bc8098169b6f8219
yinchuandong <yincd520@sina.com>
2016-12-10 09:13:02 +1100

    merge

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ STATE_DIM = 84
 STATE_CHN = 4
 ACTION_DIM = 2
 
-LOCAL_T_MAX = 32  # repeat step size
+LOCAL_T_MAX = 5  # repeat step size
 RMSP_ALPHA = 0.99  # decay parameter for RMSProp
-RMSP_EPSILON = 1e-10  # epsilon parameter for RMSProp
+RMSP_EPSILON = 0.1  # epsilon parameter for RMSProp
 GAMMA = 0.99
 ENTROPY_BETA = 0.00  # 0.01 for FFNet
 MAX_TIME_STEP = 10 * 10**7
@@ -16,7 +16,7 @@ INITIAL_ALPHA_LOG_RATE = 0.4226  # log_uniform interpolate rate for learning rat
 
 PARALLEL_SIZE = 4  # parallel thread size, please start game_server first
 USE_GPU = True
-USE_LSTM = False
+USE_LSTM = True
 
 CHECKPOINT_DIR = 'tmp/checkpoints'
 LOG_FILE = 'tmp/a3c_log'
