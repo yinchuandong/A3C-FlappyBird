c1d3e3bebda5a87addafcbfa4f0546ab1b956fb9
yinchuandong <johncdyin@gmail.com>
2017-02-03 11:12:04 +1100

    new: ------can work----

--- config.py
+++ config.py
@@ -3,20 +3,20 @@ STATE_DIM = 84
 STATE_CHN = 4
 ACTION_DIM = 2
 
-LOCAL_T_MAX = 32  # repeat step size
+LOCAL_T_MAX = 5  # repeat step size
 RMSP_ALPHA = 0.99  # decay parameter for RMSProp
-RMSP_EPSILON = 1e-10  # epsilon parameter for RMSProp
+RMSP_EPSILON = 0.1  # epsilon parameter for RMSProp
 GAMMA = 0.99
-ENTROPY_BETA = 0.0  # 0.01 for FFNet
+ENTROPY_BETA = 0.01  # 0.01 for FFNet
 MAX_TIME_STEP = 10 * 10**7
 
-INITIAL_ALPHA_LOW = 1e-5    # log_uniform low limit for learning rate
-INITIAL_ALPHA_HIGH = 1e-3   # log_uniform high limit for learning rate
+INITIAL_ALPHA_LOW = 1e-6    # log_uniform low limit for learning rate
+INITIAL_ALPHA_HIGH = 1e-4   # log_uniform high limit for learning rate
 INITIAL_ALPHA_LOG_RATE = 0.4226  # log_uniform interpolate rate for learning rate (around 7 * 10^-4)
 
 PARALLEL_SIZE = 4  # parallel thread size, please start game_server first
 USE_GPU = True
-USE_LSTM = False
+USE_LSTM = True
 
 CHECKPOINT_DIR = 'tmp/checkpoints'
 LOG_FILE = 'tmp/a3c_log'
