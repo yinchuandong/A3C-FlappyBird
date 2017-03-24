59c46a7f72bb2a9dbd34caef03f5bf907a2f796f
yinchuandong <johncdyin@gmail.com>
2017-03-25 10:54:45 +1100

    update: hyperparameters

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ LOCAL_T_MAX = 5  # repeat step size
 RMSP_ALPHA = 0.99  # decay parameter for RMSProp
 RMSP_EPSILON = 0.1  # epsilon parameter for RMSProp
 GAMMA = 0.99
-ENTROPY_BETA = 0.000001  # 0.01 for FFNet
+ENTROPY_BETA = 0.000000000001  # 0.01 for FFNet
 MAX_TIME_STEP = 10 * 10**7
 
-INITIAL_ALPHA_LOW = 1e-6   # log_uniform low limit for learning rate
-INITIAL_ALPHA_HIGH = 1e-4   # log_uniform high limit for learning rate
+INITIAL_ALPHA_LOW = 1e-4   # log_uniform low limit for learning rate
+INITIAL_ALPHA_HIGH = 1e-2   # log_uniform high limit for learning rate
 INITIAL_ALPHA_LOG_RATE = 0.4226  # log_uniform interpolate rate for learning rate (around 7 * 10^-4)
 
-PARALLEL_SIZE = 1  # parallel thread size, please start game_server first
+PARALLEL_SIZE = 4  # parallel thread size, please start game_server first
 USE_GPU = True
 USE_LSTM = True
 LSTM_UNITS = 256
